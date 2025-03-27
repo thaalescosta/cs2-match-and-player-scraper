e31fe5e1977febd74eb6d8358a7a25528e50445a
Thales Costa <thaalescosta@gmail.com>
2025-03-27 19:00:50 -0300

    added killed player x, y and z coordinates to kills.csv

--- ExtractMatchData/_functions/chromelib.py
+++ ExtractMatchData/_functions/chromelib.py
@@ -15,7 +15,7 @@ def get_chrome_options():
     root = os.path.join(os.getcwd(), r'ExtractMatchData')
     
     chrome_options = Options()        
-    chrome_options.add_argument('--headless=new')
+    # chrome_options.add_argument('--headless=new')
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36")
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument('--log-level=3')
